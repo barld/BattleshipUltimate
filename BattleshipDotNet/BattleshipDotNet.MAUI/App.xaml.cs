e76b13a1223fc477ddde5f83901cd2582b21f302
Barld Boot <bakeds@gmail.com>
2022-11-19 16:13:41 +0100

    Add default/empty maui project

---
+++ BattleshipDotNet/BattleshipDotNet.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace BattleshipDotNet.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
